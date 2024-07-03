f3dc88aff1b2c52dcb58f021aabac8a22a5c6925
Alexander Ofori <alexanderofori07@gmail.com>
2024-07-03 14:10:54 +0000

    Images Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '120': '82.4rem'
+      }
+    },
   },
   plugins: [],
 }
